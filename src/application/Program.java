0891b6caf24e4316ba9c66856b1d40dc43f89f76
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-02 21:43:52 -0300

    Começando a implementação do Tabuleiro e das peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
-import Boardgame.Position;
+import Boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		Board board = new Board(8, 8);
+		
 	}
 
 }
